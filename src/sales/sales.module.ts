75dc505fca2f50d6bc03191628f6da4afee8cc4c
Santiago Reyes Buitrago <santiagoreyesbuitrago02@gmail.com>
2024-12-13 02:15:35 -0500

    fix module sales

---
+++ src/sales/sales.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { SalesService } from './sales.service';
+import { SalesController } from './sales.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Sale, SaleSchema } from './schema/sale.schema';
+
+@Module({
+  imports: [MongooseModule.forFeature([
+      {
+        name: Sale.name,
+        schema: SaleSchema,
+      }
+    ])],
+  controllers: [SalesController],
+  providers: [SalesService],
+})
+export class SalesModule {}
